72fe7dac9c1d3ee16149d5be59b695284d48e01d
Chris Aylen <22746292+ChrisAylen@users.noreply.github.com>
2022-06-18 20:32:32 +0100

    Fixed a bug where deleteing a non existing record would exit the server

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -108,19 +108,19 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try{
-    const productData=await Product.destroy({
+    const productData= await Product.destroy({
       where:{
         id:req.params.id
       }
-    })
+    });
     if(!productData){
-      res.status(404).json({message:"Product not found"})
+      res.status(404).json({message:"Product not found" })
+      return;
     }
     res.status(200).json(productData);
-  }catch(err){
+  }
+  catch(err){
     res.status(500).json(err);
-
-    
   }
 });
 
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -71,6 +71,7 @@ router.delete('/:id', async (req, res) => {
       res.status(404).json({
         message: 'Tag not found'
       });
+      return;
     }
 
     res.status(200).json(tagData);
