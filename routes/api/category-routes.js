17b7ca293b53a4913ab1bbc50ac625573ff201f6
Chris Aylen <22746292+ChrisAylen@users.noreply.github.com>
2022-06-18 16:36:04 +0100

    Adding category put and pos  routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -20,7 +20,7 @@ router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findByPk(req.params.id,{
+    const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
     res.status(200).json(categoryData);
@@ -30,12 +30,32 @@ router.get("/:id", async (req, res) => {
 });
 
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
+
+  }
+  catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const newCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(newCategory);
+
+  }
+  catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete("/:id", (req, res) => {
